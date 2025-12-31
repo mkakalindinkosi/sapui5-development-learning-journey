2f994fdc4010206d5fcdf4f7228766db8e7329ac
Lindinkosi Mkaka <mkakalindinkosi@gmail.com>
2025-12-31 16:56:16 +0000

    Add dialog fragment, app view, and fix overview bindings

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/syncStyleClass",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,syncStyleClass,JSONModel) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
+            onInit: function () {
+            var oModel = new JSONModel();
+            this.getView().setModel(oModel, "customer");
+        },
+            onSave: function () {
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "sap.training.exc.view.Dialog"
+                    }).then(function (oDialog) {
+                        syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
+                        return oDialog;
+                        }.bind(this));
+                }
+                this.pDialog.then(function (oDialog) {
+                    oDialog.open();
+                });
+            },
 
+            onCloseDialog: function () {
+                this.byId("dialog").close();
+            },
+            onCustomerChange: function (oEvent) {
+                var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+                this.byId("bookingTable").setBindingContext(oBindingContext);
+           }
 
         });
     });
