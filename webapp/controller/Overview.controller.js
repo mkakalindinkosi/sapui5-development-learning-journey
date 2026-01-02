f765f42d83c4bea29a18baa322975681f6042f89
Lindinkosi Mkaka <mkakalindinkosi@gmail.com>
2026-01-02 05:16:59 +0000

    Fix customer-booking navigation, formatter stability, and i18n setup

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,40 +1,78 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/syncStyleClass",
-    "sap/ui/model/json/JSONModel"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller,syncStyleClass,JSONModel) {
-        "use strict";
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (
+    Controller,
+    syncStyleClass,
+    JSONModel,
+    Filter,
+    FilterOperator
+) {
+    "use strict";
 
-        return Controller.extend("sap.training.exc.controller.Overview", {
-            onInit: function () {
-            var oModel = new JSONModel();
-            this.getView().setModel(oModel, "customer");
+    return Controller.extend("sap.training.exc.controller.Overview", {
+
+        onInit: function () {
+            // Model for form editing
+            this.getView().setModel(new JSONModel({}), "customer");
         },
-            onSave: function () {
-                if (!this.pDialog) {
-                    this.pDialog = this.loadFragment({
-                        name: "sap.training.exc.view.Dialog"
-                    }).then(function (oDialog) {
-                        syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
-                        return oDialog;
-                        }.bind(this));
-                }
-                this.pDialog.then(function (oDialog) {
-                    oDialog.open();
-                });
-            },
 
-            onCloseDialog: function () {
-                this.byId("dialog").close();
-            },
-            onCustomerChange: function (oEvent) {
-                var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
-                this.byId("bookingTable").setBindingContext(oBindingContext);
-           }
+        onSave: function () {
+            if (!this.pDialog) {
+                this.pDialog = this.loadFragment({
+                    name: "sap.training.exc.view.Dialog"
+                }).then(function (oDialog) {
+                    syncStyleClass(
+                        this.getOwnerComponent().getContentDensityClass(),
+                        this.getView(),
+                        oDialog
+                    );
+                    return oDialog;
+                }.bind(this));
+            }
 
-        });
+            this.pDialog.then(function (oDialog) {
+                oDialog.open();
+            });
+        },
+
+        onCloseDialog: function () {
+            this.byId("dialog").close();
+        },
+
+        onCustomerChange: function (oEvent) {
+            var oItem = oEvent.getParameter("listItem");
+            if (!oItem) {
+                return;
+            }
+
+            var oContext = oItem.getBindingContext();
+
+            // 🔑 THIS LINE MAKES BOOKINGS WORK
+            this.byId("bookingTable").setBindingContext(oContext);
+
+            // Fill edit form
+            this.getView()
+                .getModel("customer")
+                .setData(oContext.getObject());
+        },
+
+        onFilterCustomers: function (oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var aFilters = [];
+
+            if (sQuery) {
+                aFilters.push(
+                    new Filter("CustomerName", FilterOperator.Contains, sQuery)
+                );
+            }
+
+            this.byId("customerTable")
+                .getBinding("items")
+                .filter(aFilters);
+        }
     });
+});
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,21 +1,61 @@
-sap.ui.define([], function () {
-  "use strict";
+sap.ui.define([
+    "sap/ui/model/resource/ResourceModel"
+], function (ResourceModel) {
+    "use strict";
 
-  return {
+    // ✅ Create ONE reusable ResourceBundle
+    var oResourceModel = new ResourceModel({
+        bundleName: "sap.training.exc.i18n.i18n"
+    });
 
-    classText: function (sClass) {
-      switch (sClass) {
-        case "C":
-          return "Business Class";
-        case "Y":
-          return "Economy Class";
-        case "F":
-          return "First Class";
-        default:
-          return sClass;
-      }
-    }
+    var oBundle = oResourceModel.getResourceBundle();
 
-  };
+    return {
 
+        /* ===============================
+           Flight class text
+           =============================== */
+        classText: function (sClass) {
+            if (!sClass) {
+                return "";
+            }
+
+            switch (sClass) {
+                case "C":
+                    return oBundle.getText("flightClassC");
+                case "Y":
+                    return oBundle.getText("flightClassY");
+                case "F":
+                    return oBundle.getText("flightClassF");
+                default:
+                    return sClass;
+            }
+        },
+
+        /* ===============================
+           Cancellation tooltip
+           =============================== */
+        cancellationTooltip: function (sIsCancelled) {
+            return sIsCancelled === "X"
+                ? oBundle.getText("cancelledTooltip")
+                : oBundle.getText("notCancelledTooltip");
+        },
+
+        /* ===============================
+           Flight date formatting
+           =============================== */
+        formatFlightDate: function (sDate) {
+            if (!sDate) {
+                return "";
+            }
+
+            var oDate = new Date(sDate);
+
+            return oDate.toLocaleDateString(undefined, {
+                year: "numeric",
+                month: "short",
+                day: "numeric"
+            });
+        }
+    };
 });
